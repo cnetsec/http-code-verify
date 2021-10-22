b782c17cd7035bbfaf6cb8d3a651ef2726f44587
cursosnetsec <86935257+cursosnetsec@users.noreply.github.com>
2021-10-22 16:22:26 -0300

    Update code.sh
    Ajuste Ipv6

--- code.sh
+++ code.sh
@@ -18,6 +18,6 @@ WEBSITEHTTP=$(echo "http://$line")
 WEBSITEHTTPS=$(echo "https://$line")
 HTTP=$(echo | curl -sL -w "%{http_code}\\n" $WEBSITEHTTP -o /dev/null)
 HTTPS=$(echo | curl -sL -w "%{http_code}\\n" $WEBSITEHTTPS -o /dev/null)
-IP=$(echo| nslookup $line | awk '/^Address/{n++; if (n==2){print $2; exit}}')
+IP=$(echo| nslookup $line | awk '/^Address/{n++; if (n==2) {print $2}; if (n==3){print $2}; if (n==4){exit}}')
 echo "O site $line ip $IP retorna o codigo $HTTP(HTTP) e $HTTPS(HTTPS)"
 done <"$input"
