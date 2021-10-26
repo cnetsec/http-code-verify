20f26840739b1d7c70ec822c4eb01aca3584a807
cursosnetsec <86935257+cursosnetsec@users.noreply.github.com>
2021-10-26 15:19:21 -0300

    Update code.sh
    Ajuste nos códigos

--- code.sh
+++ code.sh
@@ -17,7 +17,8 @@ echo "---------------------------------------------------------------------"
 WEBSITEHTTP=$(echo "http://$line")
 WEBSITEHTTPS=$(echo "https://$line")
 HTTP=$(echo | curl -s -w "%{http_code}\\n" $WEBSITEHTTP -o /dev/null)
-HTTPS=$(echo | curl -s -w "%{http_code}\\n" $WEBSITEHTTPS -o /dev/null)
-IP=$(echo| nslookup $line | awk '/^Address/{n++; if (n==2) {print $2}; if (n==3){print $2}; if (n==4){exit}}')
+HTTPS=$(echo | curl -sL -w "%{http_code}\\n" $WEBSITEHTTPS -o /dev/null)
+domain=$(echo $line | awk -F[/:] '{print $1}')
+IP=$(echo| nslookup $domain | awk '/^Address/{n++; if (n==2) {print $2}; if (n==3){print $2}; if (n==4){exit}}')
 echo "O site $line ip $IP retorna o codigo $HTTP(HTTP) e $HTTPS(HTTPS)"
 done <"$input"
