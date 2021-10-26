25498e0d0a7eedb7277266ab8a993dde4f832120
cursosnetsec <86935257+cursosnetsec@users.noreply.github.com>
2021-10-26 15:07:19 -0300

    Create code.sh
    Mudança Curl

--- code.sh
+++ code.sh
@@ -16,8 +16,8 @@ do
 echo "---------------------------------------------------------------------"
 WEBSITEHTTP=$(echo "http://$line")
 WEBSITEHTTPS=$(echo "https://$line")
-HTTP=$(echo | curl -sL -w "%{http_code}\\n" $WEBSITEHTTP -o /dev/null)
-HTTPS=$(echo | curl -sL -w "%{http_code}\\n" $WEBSITEHTTPS -o /dev/null)
+HTTP=$(echo | curl -s -w "%{http_code}\\n" $WEBSITEHTTP -o /dev/null)
+HTTPS=$(echo | curl -s -w "%{http_code}\\n" $WEBSITEHTTPS -o /dev/null)
 IP=$(echo| nslookup $line | awk '/^Address/{n++; if (n==2) {print $2}; if (n==3){print $2}; if (n==4){exit}}')
 echo "O site $line ip $IP retorna o codigo $HTTP(HTTP) e $HTTPS(HTTPS)"
 done <"$input"
