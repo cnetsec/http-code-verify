6fb966cebc8cc22c0af16465d4364a8ca485c64f
cursosnetsec <86935257+cursosnetsec@users.noreply.github.com>
2021-10-27 10:36:28 -0300

    Update code.sh

--- code.sh
+++ code.sh
@@ -21,4 +21,4 @@ HTTPS=$(echo | curl -sL -w "%{http_code}\\n" $WEBSITEHTTPS -o /dev/null)
 domain=$(echo $line | awk -F[/:] '{print $1}')
 IP=$(echo| nslookup $domain | awk '/^Address/{n++; if (n==2) {print $2}; if (n==3){print $2}; if (n==4){exit}}')
 echo "O site $line ip $IP retorna o codigo $HTTP(HTTP) e $HTTPS(HTTPS)"
-done <"$input
+done <$input
